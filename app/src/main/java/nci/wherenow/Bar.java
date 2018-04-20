a322b0432585adf2d3c951ef664ca140cf9aeac1
Macker67 <andrewpmcb@gmail.com>
2018-04-20 15:28:38 +0100

    Add files via upload

---
+++ app/src/main/java/nci/wherenow/Bar.java
@@ -0,0 +1,45 @@
+package nci.wherenow;
+
+
+public class Bar {
+    private int id;
+    private String title;
+    private  String address;
+    private  String distance;
+    private  String duration;
+    private  int image;
+
+
+    public Bar(int id, String title, String address, String distance, String duration, int image) {
+        this.id = id;
+        this.title = title;
+        this.address = address;
+        this.distance = distance;
+        this.duration = duration;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public  String getTitle() {
+        return title;
+    }
+
+    public  String getAddress() {
+        return address;
+    }
+
+    public  String getDistance() {
+        return distance;
+    }
+
+    public  String getDuration() {
+        return duration;
+    }
+
+    public  int getImage() {
+        return image;
+    }
+}
